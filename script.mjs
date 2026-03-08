80352bdf53f1823ffef774f30b109cc873f63eff
Ifeoma <osegboifeoma3@gmail.com>
2026-03-08 16:45:37 +0100

    created the page element for the DOM

--- script.mjs
+++ script.mjs
@@ -1,2 +1,11 @@
 import { getData, addData } from "./storage.mjs";
 import { getUserIds } from "./common.mjs";
+
+//DOM ELEMENTS
+
+const userSelect = document.getElementById("user-select");
+const agendaContainer = document.getElementById("agenda-container");
+
+const form = document.getElementById("newTopic-form");
+const topicInput = document.getElementById("newTopic-name");
+const dateInput = document.getElementById("newTopic-date");

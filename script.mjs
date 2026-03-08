f823f8c5c499e2ecf604a6b6cb610fb06e18ee0d
Ifeoma <osegboifeoma3@gmail.com>
2026-03-08 16:48:39 +0100

    created a function to populate the user selection dropdown.

--- script.mjs
+++ script.mjs
@@ -15,3 +15,19 @@ const dateInput = document.getElementById("newTopic-date");
 const today = new Date().toISOString().split("T")[0];
 dateInput.value = today;
 
+// Populate the user select dropdown
+
+userSelect.addEventListener("change", async () => {
+  const userId = userSelect.value;
+
+  if (!userId) {
+    agendaContainer.innerHTML =
+      "<p>Please select a user to view their revision agenda.</p>";
+    return;
+  }
+
+  const data = await getData(userId);
+
+  renderAgenda(data);
+});
+
